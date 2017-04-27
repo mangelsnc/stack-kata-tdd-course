965bd867c91fd295605b82c7c425062017b50312
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2017-04-27 20:07:18 +0200

    Pass first scenario

--- src/Stack/Stack.php
+++ src/Stack/Stack.php
@@ -7,5 +7,29 @@ namespace Stack;
  */
 class Stack
 {
+    public function __construct()
+    {
+
+    }
+
+    public function count()
+    {
+        return 0;
+    }
+
+    public function push($element)
+    {
+
+    }
+
+    public function pop()
+    {
+
+    }
+
+    public function getPointer()
+    {
+        
+    }
 }
 
