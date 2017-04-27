37f6ab8a68a325d88f188e646db212131aefb769
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2017-04-27 20:41:09 +0200

    Refactor, extract methods

--- src/Stack/Stack.php
+++ src/Stack/Stack.php
@@ -13,40 +13,74 @@ class Stack
     /** @var  int */
     private $pointer;
 
+    /**
+     * Stack constructor.
+     */
     public function __construct()
     {
         $this->stack = [];
         $this->pointer = -1;
     }
 
+    /**
+     * @return int
+     */
     public function count()
     {
         return count($this->stack);
     }
 
+    /**
+     * @param $element
+     */
     public function push($element)
     {
         $this->stack[] = $element;
-        $this->pointer++;
+        $this->increasePointer();
 
     }
 
+    /**
+     * @return int
+     * @throws EmptyStackException
+     */
     public function pop()
     {
-        if (0 === $this->count()) {
+        if ($this->isEmpty()) {
             throw new EmptyStackException();
         }
 
         $element = $this->stack[$this->getPointer()];
         unset($this->stack[$this->getPointer()]);
-        $this->pointer--;
+        $this->decreasePointer();
 
         return $element;
     }
 
+    /**
+     * @return int
+     */
     public function getPointer()
     {
         return $this->pointer;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEmpty()
+    {
+        return 0 === $this->count();
+    }
+
+    private function decreasePointer()
+    {
+        $this->pointer--;
+    }
+
+    private function increasePointer()
+    {
+        $this->pointer++;
     }
 }
 
